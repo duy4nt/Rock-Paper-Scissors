8fe8d16e942aea842c9af85e0dd714ee4c0f926d
duy4nt <singhduyant@gmail.com>
2025-10-23 14:06:55 +0000

    Added the game logic

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import random
+import sys
 import csv
 
 items = []
@@ -21,9 +22,60 @@ else:
 scores = items[1]
 
 print("----------Rock-Paper-Scissors----------")
-print("------",scores[0] ,"Wins", scores[1],"Losses", scores[2],"Ties", "------")
 
-usr_inp = (input("Enter your move: (r)ock (p)aper (s)cissors or (q)uit"))
 
-print(usr_inp)
+while True:
+    print('----',scores[0] ,'Wins', scores[1],'Losses', scores[2],'Ties', "-----")
+    while True:
+            print("Enter your move: (r)ock (p)aper (s)cissors or (q)uit")
+            player_move = input(">")
 
+            if player_move == "q":
+                sys.exit()
+                # TODO
+
+            if player_move == 'r' or player_move == 'p' or player_move == 's':
+                break
+
+            print('Type one of r, p, s, or q.')
+
+    if player_move == 'r':
+        print('ROCK versus...')
+    elif player_move == 'p':
+        print('PAPER versus...')
+    elif player_move == 's':
+        print('SCISSORS versus...')
+
+    move_number = random.randint(1, 3)
+    if move_number == 1:
+        computer_move = 'r'
+        print('ROCK')
+    elif move_number == 2:
+        computer_move = 'p'
+        print('PAPER')
+    elif move_number == 3:
+        computer_move = 's'
+        print('SCISSORS')
+
+    if computer_move == player_move:
+        print('Its a tie')
+        scores[2] += 1
+    elif player_move == 'r' and computer_move == 's':
+        print('You win!')
+        scores[0] += 1
+    elif player_move == 'p' and computer_move == 'r':
+        print('You win!')
+        scores[0] += 1
+    elif player_move == 's' and computer_move == 'p':
+        print('You win!')
+        scores[0] += 1
+    elif player_move == 'r' and computer_move == 'p':
+        print('You lose!')
+        scores[1] -= 1
+    elif player_move == 'p' and computer_move == 's':
+        print('You lose!')
+        scores[1] -= 1
+    elif player_move == 's' and computer_move == 'r':
+        print('You lose!')
+        scores[1] -= 1
+     
